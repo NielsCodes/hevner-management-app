78e8d454f98f84949435fbfecaa9502d8511b01c
Niels Kersic <apps@niels.codes>
2019-11-02 16:03:40 +0100

    Completed password reset page
    Added text, entry and button for password reset. User is navigated back to the login page after resetting

--- HevnerApp/HevnerApp/PasswordResetPage.xaml.cs
+++ HevnerApp/HevnerApp/PasswordResetPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace HevnerApp
@@ -15,6 +17,17 @@ namespace HevnerApp
         public PasswordResetPage()
         {
             InitializeComponent();
+
+            Title = "Reset wachtwoord";
+
+            // Resize viewport when keyboard is opened (Prevents keyboard from blocking inputs)
+            App.Current.On<Android>().UseWindowSoftInputModeAdjust(WindowSoftInputModeAdjust.Resize);
+        }
+
+        private void ResetPasswordButton_OnClicked(object sender, EventArgs e)
+        {
+            // Return to login page
+            Navigation.PopToRootAsync();
         }
     }
 }
