b1c73ab7f8b4b9673976cfe8be372557740dafea
Niels Kersic <apps@niels.codes>
2019-11-05 02:09:53 +0100

    Added Task detail page
    With Task name as title, description, status and subtasks on the page

---
+++ HevnerApp/HevnerApp/TaskDetailPage.xaml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Task = HevnerApp.Models.Task;
+
+namespace HevnerApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TaskDetailPage : ContentPage
+    {
+        public TaskDetailPage(Task task)
+        {
+            InitializeComponent();
+
+            Title = task.Name;
+            descriptionLabel.Text = task.Description;
+
+            SubtaskListView.ItemsSource = task.Subtasks;
+        }
+
+        private void SubtaskListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShowHelpToolbarItem_OnClicked(object sender, EventArgs e)
+        {
+//            Navigation
+        }
+    }
+}
